2f36d99aa78f1c6610651ec100aec0dfee3f0dcc
Ogie Galicia <o.galicia.cpa@gmail.com>
2026-02-17 13:35:40 +0800

    fix: ensure app page scroll at top

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,15 @@
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.png';
 	import { ModeWatcher } from 'mode-watcher';
+	import { afterNavigate } from '$app/navigation';
 
 	let { children } = $props();
+
+	afterNavigate((navigation) => {
+		if (navigation.from?.url.pathname !== navigation.to?.url.pathname) {
+			window.scrollTo({ top: 0, behavior: 'instant' });
+		}
+	});
 </script>
 
 <svelte:head>
